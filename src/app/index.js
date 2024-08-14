4bf2fc6b3cd3502b3b4a6859a159236a5ee7b27d
pardiniz <159555264+pardiniz@users.noreply.github.com>
2024-08-14 15:38:25 +0000

    [feat] aula 4

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from 'react-native';
+import { useAuth } from "../hooks/Auth";
 
 export default function App() {
+  const {signIn, signOut} = useAuth();
   return(
     <View style={styles.container}>
-      <Text style={styles.title}>Minha Primeira Janela</Text>
+      <Text style={styles.title}>Aplicativo Pronto para Usar</Text>
+      <Button title="Signin Super" onPress={()=>signIn({email: "super@email.com", password: "Super123!"})}/>
+      <Button title="Signin Adm" onPress={()=>signIn({email: "adm@email.com", password: "adm123!"})}/>
+      <Button title="Signin User" onPress={()=>signIn({email: "user@email.com", password: "user123!"})}/>
+      <Button title="Signout" onPress={()=>signOut()} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/hooks/Auth/index.js
@@ -0,0 +1,72 @@
+import { createContext, useEffect } from "react";
+
+const AuthContext = createContext({})
+
+export const Role = {
+    SUPER: "SUPER",
+    ADM: "ADM",
+    USER: "USER",
+
+}
+
+export function AuthProvider({children}) {
+
+  const [user, setUser] = useState({
+    autenticated: null,
+    user: null,
+    role: null,
+  });
+
+  const signIn = async ({email, password}) => {
+
+    if (email === "super@email.com" && password === "Super123!") {
+       setUser({
+        autenticated: true,
+         user:{id: 1, name: "Super Usuar", email},
+         role: Role.SUPER,
+      });
+      
+    } else if (email === "adm@email.com" && password === "adm123!") {
+      setUser({
+        autenticated: true,
+         user:{id: 2, name: "Administrador", email},
+         role: Role.ADM,
+      });
+
+    } else if(email === "user@email.com" && password === "user123!") {
+      setUser({
+        autenticated: false,
+         user: null,
+         role: null,
+      });
+  
+    }
+   };
+  
+
+   const signOut = async () => {
+    setUser({});
+   };
+
+   useEffect(()=>{
+    console.log('AuthContext: ', user)
+   }, [user]);
+
+   
+
+
+    return (
+    <AuthContext.Provider value={{user, signIn, signOut}}>
+        {children}
+    </AuthContext.Provider>
+   
+);
+   }
+
+   export function useAuth() {
+      const context = useContext(AuthContext);
+      if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+      }
+      return context;
+   }
