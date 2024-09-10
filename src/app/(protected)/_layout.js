f4e9035502eda134cd83797b5e6b6a616dcf853a
pardiniz <159555264+pardiniz@users.noreply.github.com>
2024-09-10 19:56:38 +0000

    [feat] layout

--- src/app/(protected)/_layout.js
+++ src/app/(protected)/_layout.js
@@ -1,14 +1,38 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
+import { Text } from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 const DrawerLayout = () => {
-  return <Drawer />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen 
+        name="index"
+         options={{
+          drawerLabel: "Principal",
+           headerTitle: "Principal", 
+           drawerIcon: ()=>  <Ionicons name="home-outline" size={20} color="black" />,
+           }}
+       />
+        <Drawer.Screen
+         name="list" 
+        options={{
+          drawerLabel: "Listagem", 
+          headerTitle: "Listagem",
+          drawerIcon: ()=> <Ionicons name="list-outline" size={20} color="black" />, }}
+         />
+        <Drawer.Screen
+         name="payment" 
+        options={{drawerLabel: "Pagamentos",
+           headerTitle: "Pagamentos", 
+           drawerIcon: ()=> <Ionicons name="diamond-outline" size={20} color="black" />,}} 
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 };
 
 export default function Layout() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <DrawerLayout />
-    </GestureHandlerRootView>
-  );
+  return DrawerLayout();
 }
