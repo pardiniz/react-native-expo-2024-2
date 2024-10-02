02d9b4679025333fdc58a5d714e64b2a120f7ab3
pardiniz <159555264+pardiniz@users.noreply.github.com>
2024-10-02 15:27:56 +0000

    [feat] erros corrigidos aula 12

--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -1,169 +1,63 @@
 import { router } from "expo-router";
 import { useState } from "react";
-import { Text, View, StyleSheet, InputAccessoryView } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Payment() {
   const [valor, setValor] = useState("0,00");
-  const [sugestoes, setSugestoes] = useState(
-    {
-      id: 1,
-      nome: "Vittorio Garnson",
-    },
-    {
-      id: 2,
-      nome: "Zeke Lorek",
-    },
-    {
-      id: 3,
-      nome: "Edie McOrkil",
-    },
-    {
-      id: 4,
-      nome: "Madella Sambles",
-    },
-    {
-      id: 5,
-      nome: "Nolie Morrel",
-    },
-    {
-      id: 6,
-      nome: "Rustin Skully",
-    },
-    {
-      id: 7,
-      nome: "Stephana Lockton",
-    },
-    {
-      id: 8,
-      nome: "Hamil Worton",
-    },
-    {
-      id: 9,
-      nome: "Gregor Edden",
-    },
-    {
-      id: 10,
-      nome: "Merle Siddele",
-    },
-    {
-      id: 11,
-      nome: "Byram Hourihane",
-    },
-    {
-      id: 12,
-      nome: "Jeanelle Reddy",
-    },
-    {
-      id: 13,
-      nome: "Newton Casiroli",
-    },
-    {
-      id: 14,
-      nome: "Piper Simonian",
-    },
-    {
-      id: 15,
-      nome: "Kalindi Mattisson",
-    },
-    {
-      id: 16,
-      nome: "Kordula De Matteis",
-    },
-    {
-      id: 17,
-      nome: "Joellyn Eastman",
-    },
-    {
-      id: 18,
-      nome: "Ki Winsborrow",
-    },
-    {
-      id: 19,
-      nome: "Bellina Bedinham",
-    },
-    {
-      id: 20,
-      nome: "Addie Duncan",
-    },
-    {
-      id: 21,
-      nome: "Deck Frane",
-    },
-    {
-      id: 22,
-      nome: "Katey Dobby",
-    },
-    {
-      id: 23,
-      nome: "Dalila Kirtley",
-    },
-    {
-      id: 24,
-      nome: "Jaquith Sineath",
-    },
-    {
-      id: 25,
-      nome: "Scot Annis",
-    },
-    {
-      id: 26,
-      nome: "Elfrida Widmore",
-    },
-    {
-      id: 27,
-      nome: "Emylee Verrill",
-    },
-    {
-      id: 28,
-      nome: "Waneta Hiner",
-    },
-    {
-      id: 29,
-      nome: "Paco Avesque",
-    },
-    {
-      id: 30,
-      nome: "Viki Wrigley",
-    },
-    {
-      id: 31,
-      nome: "Lorens Gherardi",
-    },
-    {
-      id: 32,
-      nome: "Phil Aleevy",
-    },
-    {
-      id: 33,
-      nome: "Georgy Cuchey",
-    },
-    {
-      id: 34,
-      nome: "Olimpia Pawelski",
-    },
-    {
-      id: 35,
-      nome: "Rivy Dorrell",
-    }
-  );
+  const [sugestoes, setSugestoes] = useState([
+    { id: 1, nome: "Vittorio Garnson" },
+    { id: 2, nome: "Zeke Lorek" },
+    { id: 3, nome: "Edie McOrkil" },
+    { id: 4, nome: "Madella Sambles" },
+    { id: 5, nome: "Nolie Morrel" },
+    { id: 6, nome: "Rustin Skully" },
+    { id: 7, nome: "Stephana Lockton" },
+    { id: 8, nome: "Hamil Worton" },
+    { id: 9, nome: "Gregor Edden" },
+    { id: 10, nome: "Merle Siddele" },
+    { id: 11, nome: "Byram Hourihane" },
+    { id: 12, nome: "Jeanelle Reddy" },
+    { id: 13, nome: "Newton Casiroli" },
+    { id: 14, nome: "Piper Simonian" },
+    { id: 15, nome: "Kalindi Mattisson" },
+    { id: 16, nome: "Kordula De Matteis" },
+    { id: 17, nome: "Joellyn Eastman" },
+    { id: 18, nome: "Ki Winsborrow" },
+    { id: 19, nome: "Bellina Bedinham" },
+    { id: 20, nome: "Addie Duncan" },
+    { id: 21, nome: "Deck Frane" },
+    { id: 22, nome: "Katey Dobby" },
+    { id: 23, nome: "Dalila Kirtley" },
+    { id: 24, nome: "Jaquith Sineath" },
+    { id: 25, nome: "Scot Annis" },
+    { id: 26, nome: "Elfrida Widmore" },
+    { id: 27, nome: "Emylee Verrill" },
+    { id: 28, nome: "Waneta Hiner" },
+    { id: 29, nome: "Paco Avesque" },
+    { id: 30, nome: "Viki Wrigley" },
+    { id: 31, nome: "Lorens Gherardi" },
+    { id: 32, nome: "Phil Aleevy" },
+    { id: 33, nome: "Georgy Cuchey" },
+    { id: 34, nome: "Olimpia Pawelski" },
+    { id: 35, nome: "Rivy Dorrell" },
+  ]);
   const [id, setId] = useState(1);
   const [data, setData] = useState(new Date());
   const [viewCalendar, setViewCalendar] = useState(false);
-  const [observacoes, setObservacao] = useState("");
+  const [observacao, setObservacao] = useState("");
 
   const handleCalendar = (event, selectedDate) => {
-    setData(selectDate);
+    setData(selectedDate);
     setViewCalendar(false);
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.content}>
       <Text>Inserir Pagamentos</Text>
-      <View style={styles.InputView}>
+      <View style={styles.inputView}>
         <Ionicons name="wallet-outline" size={24} color="black" />
         <TextInput
           placeholder="Valor"
@@ -173,28 +67,20 @@ export default function Payment() {
           onChangeText={setValor}
         />
       </View>
-      <View style={styles.InputView}>
+      <View style={styles.inputView}>
         <Picker
           selectedValue={id}
-          onValueChange={(itemValue, index) => {
-            setId(itemValue);
-          }}
+          onValueChange={(itemValue) => setId(itemValue)}
           style={{ width: "100%" }}
         >
-          {sugestoes?.map(() => {
-            return (
-              <Picker.Item key={item.id} label={item.nome} value={item.id} />
-            );
-          })}
+          {sugestoes.map((item) => (
+            <Picker.Item key={item.id} label={item.nome} value={item.id} />
+          ))}
         </Picker>
-        <Text
-          onPress={() => setViewCalendar(true)}
-          style={styles.inputObservacao}
-          value={observacao}
-          onChangeText={setObservacao}
-          multiline={true}
-        >
-          {data.toLocaleDateString().split("T")[0]}
+      </View>
+      <View style={styles.inputView}>
+        <Text onPress={() => setViewCalendar(true)} style={styles.inputData}>
+          {data.toLocaleDateString()}
         </Text>
         {viewCalendar && (
           <DateTimePicker
@@ -204,14 +90,15 @@ export default function Payment() {
             testID="dateTimePicker"
           />
         )}
-
-        <TextInput placeholder="Usuario" />
       </View>
-      <View style={styles.InputView}>
-        <TextInput placeholder="Data" />
-      </View>
-      <View style={styles.InputView}>
-        <TextInput placeholder="Observações" />
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Observações"
+          style={styles.inputObservacao}
+          value={observacao}
+          onChangeText={setObservacao}
+          multiline={true}
+        />
       </View>
       <View style={styles.contentButtons}>
         <Button title="Salvar" />
@@ -229,7 +116,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 10,
   },
-  InputView: {
+  inputView: {
     borderColor: "black",
     borderWidth: 1,
     width: "100%",
@@ -251,12 +138,10 @@ const styles = StyleSheet.create({
   inputData: {
     width: "100%",
     textAlign: "center",
-    fontFamily: "regular",
     fontSize: 20,
     padding: 10,
   },
   inputObservacao: {
-    fontFamily: "regular",
     fontSize: 16,
     flex: 1,
     lineHeight: 20,
